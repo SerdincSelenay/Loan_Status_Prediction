e9b3e9564c7a08685ae3297ae667cb31d11944c6
Beyzotti <136998182+Beyzotti@users.noreply.github.com>
2024-04-08 22:21:43 +0300

    Add files via upload

---
+++ Loan_Prediction.py
@@ -0,0 +1,286 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import warnings
+from sklearn.exceptions import ConvergenceWarning
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier, VotingClassifier, AdaBoostClassifier
+from sklearn.model_selection import GridSearchCV, cross_validate, RandomizedSearchCV, train_test_split, cross_val_score
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from xgboost import XGBClassifier
+from lightgbm import LGBMClassifier
+from sklearn.preprocessing import MinMaxScaler, LabelEncoder, StandardScaler, RobustScaler, OneHotEncoder
+from sklearn.svm import SVC
+from catboost import CatBoostClassifier
+import joblib
+
+
+warnings.simplefilter(action='ignore', category=FutureWarning)
+warnings.simplefilter("ignore", category=ConvergenceWarning)
+
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', None)
+pd.set_option('display.float_format', lambda x: '%.3f' % x)
+
+df = pd.read_csv("Dataset/loan_data.csv")
+df = df.drop(["Loan_ID"], axis=1)
+
+
+def check_df(dataframe):
+    print("##################### Shape #####################")
+    print(dataframe.shape)
+    print("##################### Types #####################")
+    print(dataframe.dtypes)
+    print("##################### Head #####################")
+    print(dataframe.head(3))
+    print("##################### Tail #####################")
+    print(dataframe.tail(3))
+    print("##################### NA #####################")
+    print(dataframe.isnull().sum())
+
+
+def cat_summary(dataframe, col_name, plot=False):
+    print(pd.DataFrame({col_name: dataframe[col_name].value_counts(),
+                        "Ratio": 100 * dataframe[col_name].value_counts() / len(dataframe)}))
+    print("##########################################")
+    if plot:
+        sns.countplot(x=dataframe[col_name], data=dataframe)
+        plt.show(block=True)
+
+
+def num_summary(dataframe, numerical_col, plot=False):
+    quantiles = [0.05, 0.10, 0.20, 0.30, 0.40, 0.50, 0.60, 0.70, 0.80, 0.90, 0.95, 0.99]
+    print(dataframe[numerical_col].describe(quantiles).T)
+
+    if plot:
+        dataframe[numerical_col].hist(bins=20)
+        plt.xlabel(numerical_col)
+        plt.title(numerical_col)
+        plt.show(block=True)
+
+
+def target_summary_with_num(dataframe, target, numerical_col):
+    print(dataframe.groupby(target).agg({numerical_col: "mean"}), end="\n\n\n")
+
+
+def target_summary_with_cat(dataframe, target, categorical_col):
+    print(pd.DataFrame({"TARGET_MEAN": dataframe.groupby(categorical_col)[target].mean()}), end="\n\n\n")
+
+
+def correlation_matrix(df, cols):
+    fig = plt.gcf()
+    fig.set_size_inches(10, 8)
+    plt.xticks(fontsize=10)
+    plt.yticks(fontsize=10)
+    fig = sns.heatmap(df[cols].corr(), annot=True, linewidths=0.5, annot_kws={'size': 12}, linecolor='w', cmap='RdBu')
+    plt.show(block=True)
+
+
+def grab_col_names(dataframe, cat_th=10, car_th=20):
+    """
+
+    Veri setindeki kategorik, numerik ve kategorik fakat kardinal değişkenlerin isimlerini verir.
+    Not: Kategorik değişkenlerin içerisine numerik görünümlü kategorik değişkenler de dahildir.
+
+    Parameters
+    ------
+        dataframe: dataframe
+                Değişken isimleri alınmak istenilen dataframe
+        cat_th: int, optional
+                numerik fakat kategorik olan değişkenler için sınıf eşik değeri
+        car_th: int, optinal
+                kategorik fakat kardinal değişkenler için sınıf eşik değeri
+
+    Returns
+    ------
+        cat_cols: list
+                Kategorik değişken listesi
+        num_cols: list
+                Numerik değişken listesi
+        cat_but_car: list
+                Kategorik görünümlü kardinal değişken listesi
+
+    Examples
+    ------
+        import seaborn as sns
+        df = sns.load_dataset("iris")
+        print(grab_col_names(df))
+
+
+    Notes
+    ------
+        cat_cols + num_cols + cat_but_car = toplam değişken sayısı
+        num_but_cat cat_cols'un içerisinde.
+        Return olan 3 liste toplamı toplam değişken sayısına eşittir: cat_cols + num_cols + cat_but_car = değişken sayısı
+
+    """
+
+    # cat_cols, cat_but_car
+    cat_cols = [col for col in dataframe.columns if dataframe[col].dtypes == "O"]
+    num_but_cat = [col for col in dataframe.columns if dataframe[col].nunique() < cat_th and
+                   dataframe[col].dtypes != "O"]
+    cat_but_car = [col for col in dataframe.columns if dataframe[col].nunique() > car_th and
+                   dataframe[col].dtypes == "O"]
+    cat_cols = cat_cols + num_but_cat
+    cat_cols = [col for col in cat_cols if col not in cat_but_car]
+
+    # num_cols
+    num_cols = [col for col in dataframe.columns if dataframe[col].dtypes != "O"]
+    num_cols = [col for col in num_cols if col not in num_but_cat]
+
+    # print(f"Observations: {dataframe.shape[0]}")
+    # print(f"Variables: {dataframe.shape[1]}")
+    # print(f'cat_cols: {len(cat_cols)}')
+    # print(f'num_cols: {len(num_cols)}')
+    # print(f'cat_but_car: {len(cat_but_car)}')
+    # print(f'num_but_cat: {len(num_but_cat)}')
+    return cat_cols, num_cols, cat_but_car
+
+
+check_df(df)
+
+df.describe().T
+
+# Kategorik eksikler için veri doldurma işlemi
+df["Gender"].fillna(df["Credit_History"].mode()[0], inplace=True)
+df["Dependents"].fillna(df["Credit_History"].mode()[0], inplace=True)
+df["Self_Employed"].fillna(df["Credit_History"].mode()[0], inplace=True)
+
+# Numerik eksik değerler için veri doldurma işlemi
+df["Loan_Amount_Term"].fillna(df["Loan_Amount_Term"].median(), inplace=True)
+df["Credit_History"].fillna(df["Credit_History"].mode()[0], inplace=True)
+
+# Eksik veri kontrolü
+df.isnull().sum()
+
+# Değişken türlerinin ayrıştırılması
+cat_cols, num_cols, cat_but_car = grab_col_names(df, cat_th=5, car_th=20)
+
+# Kategorik değişkenlerin incelenmesi
+for col in cat_cols:
+    cat_summary(df, col)
+
+# Sayısal değişkenlerin incelenmesi
+df[num_cols].describe().T
+
+# Sayısal değişkenkerin birbirleri ile korelasyonu
+correlation_matrix(df, num_cols)
+
+
+# Data'nın incelenmesi
+def outlier_thresholds(dataframe, col_name, q1=0.25, q3=0.75):
+    quartile1 = dataframe[col_name].quantile(q1)
+    quartile3 = dataframe[col_name].quantile(q3)
+    interquantile_range = quartile3 - quartile1
+    up_limit = quartile3 + 1.5 * interquantile_range
+    low_limit = quartile1 - 1.5 * interquantile_range
+    return low_limit, up_limit
+
+
+def replace_with_thresholds(dataframe, variable):
+    low_limit, up_limit = outlier_thresholds(dataframe, variable)
+    dataframe.loc[(dataframe[variable] < low_limit), variable] = low_limit
+    dataframe.loc[(dataframe[variable] > up_limit), variable] = up_limit
+
+
+def one_hot_encoder(dataframe, categorical_cols, drop_first=False):
+    dataframe = pd.get_dummies(dataframe, columns=categorical_cols, drop_first=drop_first)
+    return dataframe
+
+
+for i in num_cols:
+    replace_with_thresholds(df, i)
+
+df.head()
+
+df = one_hot_encoder(df, cat_cols, drop_first=True)
+
+X_scaled = StandardScaler().fit_transform(df[num_cols])
+df[num_cols] = pd.DataFrame(X_scaled, columns=df[num_cols].columns)
+
+y = df["Loan_Status_Y"]
+X = df.drop(["Loan_Status_Y"], axis=1)
+
+
+def base_models(X, y, scoring="roc_auc"):
+    print("Base Models....")
+    classifiers = [('LR', LogisticRegression()),
+                   ('KNN', KNeighborsClassifier()),
+                   ("SVC", SVC()),
+                   ("CART", DecisionTreeClassifier()),
+                   ("RF", RandomForestClassifier()),
+                   ('Adaboost', AdaBoostClassifier()),
+                   ('GBM', GradientBoostingClassifier()),
+                   ('XGBoost', XGBClassifier(use_label_encoder=False, eval_metric='logloss')),
+                   ('LightGBM', LGBMClassifier()),
+                   # ('CatBoost', CatBoostClassifier(verbose=False))
+                   ]
+
+    for name, classifier in classifiers:
+        cv_results = cross_validate(classifier, X, y, cv=3, scoring=scoring)
+        print(f"{scoring}: {round(cv_results['test_score'].mean(), 4)} ({name}) ")
+
+
+knn_params = {"n_neighbors": range(2, 50)}
+
+cart_params = {'max_depth': range(1, 20),
+               "min_samples_split": range(2, 30)}
+
+rf_params = {"max_depth": [8, 15, None],
+             "max_features": [5, 7, "auto"],
+             "min_samples_split": [15, 20],
+             "n_estimators": [200, 300]}
+
+xgboost_params = {"learning_rate": [0.1, 0.01],
+                  "max_depth": [5, 8],
+                  "n_estimators": [100, 200],
+                  "colsample_bytree": [0.5, 1]}
+
+lightgbm_params = {"learning_rate": [0.01, 0.1],
+                   "n_estimators": [300, 500],
+                   "colsample_bytree": [0.7, 1]}
+
+classifiers = [('KNN', KNeighborsClassifier(), knn_params),
+               ("CART", DecisionTreeClassifier(), cart_params),
+               ("RF", RandomForestClassifier(), rf_params),
+               ('XGBoost', XGBClassifier(use_label_encoder=False, eval_metric='logloss'), xgboost_params),
+               ('LightGBM', LGBMClassifier(), lightgbm_params)]
+
+
+def hyperparameter_optimization(X, y, cv=3, scoring="roc_auc"):
+    print("Hyperparameter Optimization....")
+    best_models = {}
+    for name, classifier, params in classifiers:
+        print(f"########## {name} ##########")
+        cv_results = cross_validate(classifier, X, y, cv=cv, scoring=scoring)
+        print(f"{scoring} (Before): {round(cv_results['test_score'].mean(), 4)}")
+
+        gs_best = GridSearchCV(classifier, params, cv=cv, n_jobs=-1, verbose=False).fit(X, y)
+        final_model = classifier.set_params(**gs_best.best_params_)
+
+        cv_results = cross_validate(final_model, X, y, cv=cv, scoring=scoring)
+        print(f"{scoring} (After): {round(cv_results['test_score'].mean(), 4)}")
+        print(f"{name} best params: {gs_best.best_params_}", end="\n\n")
+        best_models[name] = final_model
+    return best_models
+
+
+def voting_classifier(best_models, X, y):
+    print("Voting Classifier...")
+    voting_clf = VotingClassifier(estimators=[('KNN', best_models["KNN"]), ('RF', best_models["RF"]),
+                                              ('LightGBM', best_models["LightGBM"])],
+                                  voting='soft').fit(X, y)
+    cv_results = cross_validate(voting_clf, X, y, cv=3, scoring=["accuracy", "f1", "roc_auc"])
+    print(f"Accuracy: {cv_results['test_accuracy'].mean()}")
+    print(f"F1Score: {cv_results['test_f1'].mean()}")
+    print(f"ROC_AUC: {cv_results['test_roc_auc'].mean()}")
+    return voting_clf
+
+
+hyperparameter_optimization(X, y, cv=3, scoring="roc_auc")
+
+base_models(X, y)
+best_models = hyperparameter_optimization(X, y)
+voting_clf = voting_classifier(best_models, X, y)
